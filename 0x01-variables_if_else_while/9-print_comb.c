054c79823615718355ac1bf1ae6f4ccda76afe04
danomwa <danomwaopiyo@gmail.com>
2021-06-18 08:38:15 -0700

    prints all possible comb of a single digit number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,14 @@ int main(void)
 {
 	int i = 0;
 
-	for (i = 10; i < 99; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-		putchar(',');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
