cc34be96aef38309d25a18058563ddc7810dac4e
danomwa <danomwaopiyo@gmail.com>
2021-06-18 05:42:41 -0700

    prints single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Print single digt numbers of base 10 from 1
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
