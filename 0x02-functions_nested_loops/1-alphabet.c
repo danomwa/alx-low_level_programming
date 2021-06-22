f340c3d97e1b9887475b0f8bb46fd2b92ee6e207
danomwa <danomwaopiyo@gmail.com>
2021-06-22 05:01:12 -0700

    prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+}
