ab373f0d907cf6efe4c44b39aa784571cf64fcbc
danomwa <danomwaopiyo@gmail.com>
2021-06-22 08:04:10 -0700

    prints alphabet 10 times

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* 0-holberton.c */
+int _putchar(char);
+
+/*1-alphabet.c */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c*/
+void print_alphabet_x10(void)
+
+#endif
