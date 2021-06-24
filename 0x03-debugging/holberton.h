d24f0514dd69b5bec54c6caebaa7cbfd1940ce00
danomwa <danomwaopiyo@gmail.com>
2021-06-24 01:46:39 -0700

    commit

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,3 +5,12 @@
 
 /*main.c*/
 void positive_or_negative(int i);
+
+/*2-largest_number.c*/
+int largest_number(int a, int b, int c);
+
+/*3-print_remaining_days.c*/
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+#endif
