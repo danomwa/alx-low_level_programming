2208176f8ae22fc6c0944947d9b59bbf09450d2d
danomwa <danomwaopiyo@gmail.com>
2021-06-24 03:34:42 -0700

    prints a line n chars long

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -21,4 +21,19 @@ void print_most_numbers(void);
 /*5-more_numbers.c*/
 void more_numbers(void);
 
+/*6-print_line.c*/
+void print_line(int n);
+
+/*7-print_diagonal.c*/
+void print_diagonal(int n);
+
+/*8-print_squares.c*/
+void print_square(int size);
+
+/*10-print_triangle.c*/
+void print_triangle(int size);
+
+/*101-print_number.c*/
+void print_number(int n);
+
 #endif /* HOLBERTON_H*/
