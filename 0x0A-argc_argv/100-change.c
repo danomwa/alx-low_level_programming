81b1dc3162009f875f771ae6104345efd0ae1d37
danomwa <84831872+danomwa@users.noreply.github.com>
2021-07-12 18:33:48 +0300

    Create 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,68 +1,55 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the min num of coins to make change for an amount of money
- *
+ * returnCents - calculates the min number of coins needed to make change given
+ * @n: target change value
+ * Return: min num of coins
+ */
+int returnCents(int n)
+{
+	int coins = 0;
+
+	while (n)
+	{
+		if (n >= 25)
+			n -= 25;
+		else if (n >= 10)
+			n -= 10;
+		else if (n >= 5)
+			n -= 5;
+		else if (n >= 2)
+			n -= 2;
+		else if (n >= 1)
+			n -= 1;
+		coins++;
+	}
+	return (coins);
+}
+
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
  * @argc: argument count
  * @argv: argument vector
- * Return: int
+ * Return: 0 if no errors
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int count = 0;
+	int number;
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-else if (atoi(argv[1]) < 0)
-{
-printf("%d\n", 0);
-return (0);
-}
-count = coin_count(argv, count);
-printf("%d\n", count);
-return (0);
-}
-
-/**
- * coin_count - counts the min coins needed for change
- *
- * @argv: argument vector
- * @count: int to count coins
- * Return: int
- */
-int coin_count(char *argv[], int count)
-{
-int sum = 0;
-
-while (!(sum + 25 > atoi(argv[1])))
-{
-sum += 25;
-count++;
-}
-while (!(sum + 10 > atoi(argv[1])))
-{
-sum += 10;
-count++;
-}
-while (!(sum + 5 > atoi(argv[1])))
-{
-sum += 5;
-count++;
-}
-while (!(sum + 2 > atoi(argv[1])))
-{
-sum += 2;
-count++;
-}
-while (!(sum + 1 > atoi(argv[1])))
-{
-sum += 1;
-count++;
-}
-return (count);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	number = atoi(argv[1]);
+	if (number < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	printf("%i\n", returnCents(number));
+	return (0);
 }
